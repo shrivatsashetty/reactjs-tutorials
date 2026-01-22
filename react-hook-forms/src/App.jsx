1cdb6705f94ce0af123e5c57274f9d6e3a2ce19a
shrivatsashetty <shrivatsa307@gmail.com>
2026-01-22 20:14:05 +0530

    modified App.jsx to be compatible with the express backend

--- react-hook-forms/src/App.jsx
+++ react-hook-forms/src/App.jsx
@@ -1,4 +1,3 @@
-import { useFormState } from "react-dom";
 import "./App.css";
 
 import { useForm } from "react-hook-form";
@@ -11,52 +10,76 @@ function App() {
         formState: { errors, isSubmitting },
     } = useForm();
 
-	// function for simulating network delay
-	const delay = async (numSeconds) => {
-		return new Promise(
-			(resolve) => {
-				setTimeout(
-					() => {
-						resolve();
-					},
-					numSeconds * 1000
-				)
-			}
-		);
-	}
+    // function for simulating network delay
+    const delay = async (numSeconds) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, numSeconds * 1000);
+        });
+    };
+
+    async function sendResponse(data) {
+        try {
+            let response = await fetch("http://localhost:3000", {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+
+            let jsonData = await response.json();
+
+            console.log(jsonData);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     const onSubmit = async (data) => {
-		await delay(2);
-    	alert("Form Submitted, check console for data");
-		console.log(data);
+        await delay(2);
+        alert("Form Submitted, check console for data");
+        console.log(data);
 
-		if(data.username.startsWith("s") ) {
-			setError("invalidUser", {message: "Invalid username"}) // creating a custom error
-		}
+        sendResponse(data);
+
+        if (data.username.startsWith("s")) {
+            setError("invalidUser", { message: "Invalid username" }); // creating a custom error
+        }
     };
 
     return (
         <>
-			{isSubmitting && <div>Submitting Form...</div> }
+            {isSubmitting && <div>Submitting Form...</div>}
             <div className="container">
                 <form action="" onSubmit={handleSubmit(onSubmit)}>
                     <input
                         placeholder="username"
-						type="text"
-
-						// rest of the attributes for the input element can be set using the register function
-                        {
-							...register(
-									"username", 
-									{
-										required: {value: true, message: "username is Required"}, 
-										minLength: {value: 4, message: "Min 3 Characters Long"}, 
-										maxLength: {value: 10, message: "Max 10 Chars allowed"}
-									} 
-								)
-						}	
+                        type="text"
+                        // rest of the attributes for the input element can be set using the register function
+                        {...register("username", {
+                            required: {
+                                value: true,
+                                message: "username is Required",
+                            },
+                            minLength: {
+                                value: 4,
+                                message: "Min 3 Characters Long",
+                            },
+                            maxLength: {
+                                value: 10,
+                                message: "Max 10 Chars allowed",
+                            },
+                        })}
                     />
-					{errors.username &&  <div className="red">{errors.username.message}</div>}
+                    {errors.username && (
+                        <div className="red">{errors.username.message}</div>
+                    )}
 
                     <br />
                     <input
@@ -65,8 +88,14 @@ function App() {
                         type="password"
                     />
                     <br />
-                    <input  disabled={isSubmitting} type="submit" value="Submit" />
-					{errors.invalidUser &&  <div className="red">{errors.invalidUser.message}</div>}
+                    <input
+                        disabled={isSubmitting}
+                        type="submit"
+                        value="Submit"
+                    />
+                    {errors.invalidUser && (
+                        <div className="red">{errors.invalidUser.message}</div>
+                    )}
                 </form>
             </div>
         </>
